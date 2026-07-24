d6e25b706d63190197716f6e2c3c8d05aa856974
behdad <behdadbabaie@gmail.com>
2026-07-24 02:21:00 -0700

    feat(python): complete Python development environment

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "=================================="
+echo " Welcome to DevLab"
+echo "=================================="
+
+python3 --version
+uv --version
+git --version
+
+echo
+echo "Devlab is ready!"
